135a0a81e773bede352495b52f8dcd82c04fd822
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-09-26 21:23:52 +0530

    add endpoint handler to redirect to url from short url

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import connectToMongoDB from './connect';
+import urlRoute from './routes/url'
+import URL from './models/url';
 import express from "express";
-import connectToMongoDB from './utils/connect';
+import path from "node:path"
 
 const app = express();
 app.use(express.json());
+app.use('/url', urlRoute);
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +15,20 @@ connectToMongoDB("mongodb://localhost:27017/url-shortener")
   .catch(() => console.log("failed to connect to DB..."));
 
 
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate({ shortId }, {
+    $push: {
+      visitHistory: { timestamp: Date.now() },
+    },
+  });
+  if (!entry) {
+    res.status(404).send({ error: "short id not found" });
+    return;
+  }
+  console.log(entry);
+  res.redirect(entry?.redirectUrl);
+});
 
 
 app.listen(PORT, () => {
